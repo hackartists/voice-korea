00b7d4bc739fea9d1f433beda1fc106208b081ad
hackartists <hackartists@gmail.com>
2025-02-09 12:46:20 +0900

    feat(main-ui): add error handling and memoization for org_id in Controller

--- packages/main-ui/src/layouts/root_layout/mod.rs
+++ packages/main-ui/src/layouts/root_layout/mod.rs
@@ -22,11 +22,11 @@ pub fn RootLayout(lang: Language) -> Element {
 
     let tr: RootLayoutTranslate = translate(&lang);
     let user: LoginService = use_context();
-    let is_logged_in = (user.email)().clone().is_some();
+    let is_logged_in = use_signal(|| (user.email)().clone().is_some());
     let nav = use_navigator();
 
     use_effect(move || {
-        if !is_logged_in {
+        if !is_logged_in() {
             tracing::info!("redirect to login page");
 
             nav.replace(Route::LoginPage { lang });
@@ -34,35 +34,41 @@ pub fn RootLayout(lang: Language) -> Element {
     });
 
     rsx! {
-        div { class: "flex flex-col w-screen min-h-screen bg-white text-black",
-            // Header {
-            //     logout: translates.logout,
-            //     lang,
-            // }
-            PopupZone {}
-            div { class: "flex flex-row min-w-full max-w-full grow",
-                SideBar {
-                    onselected: move |selected: SelectedMenu| {
-                        tracing::info!("selected menu {selected:?}");
-                        selected_menu.set(selected.menu);
-                    },
-                    selected_menu: (selected_menu)(),
-                    lang,
-                }
-                div { class: "flex flex-col grow w-full bg-[#f0f2fc] px-[60px] pt-[25px]",
-                    div { class: "flex flex-row w-full justify-end items-end gap-[5px]",
-                        Link {
-                            class: "flex flex-row justify-start items-start",
-                            to: Route::LoginPage { lang },
-                            div { class: "w-[20px] h-[20px]",
-                                Logout { width: "20", height: "20" }
-                            }
-                            div { class: "ml-[5px] font-bold text-[#555462] text-[15px]",
-                                "{tr.logout}"
+        ErrorBoundary {
+            handle_error: move |e| {
+                tracing::error!("error: {:?}", e);
+                rsx! { "error : " }
+            },
+            div { class: "flex flex-col w-screen min-h-screen bg-white text-black",
+                // Header {
+                //     logout: translates.logout,
+                //     lang,
+                // }
+                PopupZone {}
+                div { class: "flex flex-row min-w-full max-w-full grow",
+                    SideBar {
+                        onselected: move |selected: SelectedMenu| {
+                            tracing::info!("selected menu {selected:?}");
+                            selected_menu.set(selected.menu);
+                        },
+                        selected_menu: (selected_menu)(),
+                        lang,
+                    }
+                    div { class: "flex flex-col grow w-full bg-[#f0f2fc] px-[60px] pt-[25px]",
+                        div { class: "flex flex-row w-full justify-end items-end gap-[5px]",
+                            Link {
+                                class: "flex flex-row justify-start items-start",
+                                to: Route::LoginPage { lang },
+                                div { class: "w-[20px] h-[20px]",
+                                    Logout { width: "20", height: "20" }
+                                }
+                                div { class: "ml-[5px] font-bold text-[#555462] text-[15px]",
+                                    "{tr.logout}"
+                                }
                             }
                         }
+                        Outlet::<Route> {}
                     }
-                    Outlet::<Route> {}
                 }
             }
         }
