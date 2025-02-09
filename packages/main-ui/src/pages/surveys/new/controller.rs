f6ad9ea59f1d6baae830a93d405c376977e8d2fb
hackartists <hackartists@gmail.com>
2025-02-10 00:09:59 +0900

    feat(main-ui): add num-format dependency and refactor survey panel controller
    - Added `num-format` dependency to `Cargo.toml` and updated `Cargo.lock`.
    - Refactored `Controller` to `PanelController` in `controller.rs`.
    - Updated `SettingPanel` component to use `PanelController`.
    - Added new translations in `i18n.rs`.
    - Updated `SurveyCreatePage` to handle new panel settings.

--- packages/main-ui/src/pages/surveys/new/controller.rs
+++ packages/main-ui/src/pages/surveys/new/controller.rs
@@ -2,8 +2,8 @@ use dioxus::prelude::*;
 use dioxus_logger::tracing;
 use dioxus_translate::{translate, Language};
 use models::{
-    PanelV2, PanelV2Action, PanelV2Client, PanelV2CreateRequest, PanelV2Query, PanelV2Summary,
-    ProjectArea, QueryResponse, SurveyV2,
+    PanelV2, PanelV2Action, PanelV2CreateRequest, PanelV2Query, PanelV2Summary, ProjectArea,
+    QueryResponse, SurveyV2,
 };
 
 use crate::{
@@ -17,8 +17,6 @@ use super::{create_survey::CreateSurveyResponse, i18n::SurveyNewTranslate};
 
 #[derive(Clone, Copy)]
 pub struct Controller {
-    lang: Language,
-
     selected_field: Signal<Option<ProjectArea>>,
     nav: Navigator,
     user: LoginService,
@@ -29,53 +27,16 @@ pub struct Controller {
     survey_request: Signal<Option<CreateSurveyResponse>>,
 
     panels: Signal<Vec<PanelV2Summary>>,
-    panel_resource: Resource<QueryResponse<PanelV2Summary>>,
-    client: Signal<PanelV2Client>,
-    org_id: Memo<String>,
     selected_panels: Signal<Vec<PanelV2>>,
     maximum_panel_count: Signal<Vec<u64>>,
     total_panel_members: Signal<u64>,
-    popup_service: PopupService,
-
-    page: Signal<usize>,
-    translates: Signal<SurveyNewTranslate>,
 }
 
 impl Controller {
     pub fn new(lang: dioxus_translate::Language) -> Self {
         let translates: SurveyNewTranslate = translate(&lang);
-        let login_service: LoginService = use_context();
-        let org_id = use_memo(move || match login_service.get_selected_org() {
-            Some(v) => v.id.to_string(),
-            None => "".to_string(),
-        });
-        let client = PanelV2::get_client(&crate::config::get().api_url);
-        let page = use_signal(|| 1);
 
-        let panel_resource = use_resource(move || {
-            let page = page();
-            let org_id = org_id();
-            let size = 100;
-
-            let client = PanelV2::get_client(&crate::config::get().api_url);
-
-            async move {
-                //FIMXE: fix to get total data
-                let query = PanelV2Query::new(size).with_page(page);
-                match client
-                    .query(org_id.parse::<i64>().unwrap_or_default(), query)
-                    .await
-                {
-                    Ok(d) => d,
-                    Err(e) => {
-                        tracing::error!("list panels failed: {e}");
-                        QueryResponse::default()
-                    }
-                }
-            }
-        });
-
-        let mut ctrl = Self {
+        let ctrl = Self {
             nav: use_navigator(),
             user: use_context(),
 
@@ -94,26 +55,11 @@ impl Controller {
 
             current_step: use_signal(|| CurrentStep::CreateSurvey),
             panels: use_signal(|| vec![]),
-            panel_resource,
-            client: use_signal(|| client),
-            org_id,
 
             selected_panels: use_signal(|| vec![]),
             maximum_panel_count: use_signal(|| vec![]),
             total_panel_members: use_signal(|| 0),
-
-            popup_service: use_context(),
-            lang,
-            translates: use_signal(|| translates),
-            page,
         };
-
-        let _ = use_effect(move || match panel_resource.value()() {
-            Some(panel) => {
-                ctrl.panels.set(panel.items);
-            }
-            _ => {}
-        });
 
         use_context_provider(|| ctrl);
 
@@ -124,10 +70,6 @@ impl Controller {
         tracing::debug!("handle_survey_request: {:?}", survey);
         self.survey_request.set(Some(survey));
         self.current_step.set(CurrentStep::SettingPanel);
-    }
-
-    pub fn get_page(&self) -> usize {
-        (self.page)()
     }
 
     pub fn change_step(&mut self, step: CurrentStep) {
@@ -150,62 +92,62 @@ impl Controller {
         (self.selected_panels)()
     }
 
-    pub async fn open_create_panel_modal(&self) {
-        let mut popup_service = self.popup_service;
-        let translates = (self.translates)();
-        let mut panel_resource = self.panel_resource;
-        let client = (self.client)().clone();
-        let org_id = (self.org_id)();
+    // pub async fn open_create_panel_modal(&self) {
+    //     let mut popup_service = self.popup_service;
+    //     let translates = (self.translates)();
+    //     let mut panel_resource = self.panel_resource;
+    //     let client = (self.client)().clone();
+    //     let org_id = (self.org_id)();
 
-        let mut ctrl = self.clone();
+    //     let mut ctrl = self.clone();
 
-        popup_service
-            .open(rsx! {
-                CreatePanelModal {
-                    lang: self.lang,
-                    onsave: {
-                        let client = client.clone();
-                        let org_id = org_id.clone();
-                        move |req: PanelV2CreateRequest| {
-                            let client = client.clone();
-                            let org_id = org_id.clone();
-                            async move {
-                                match client
-                                    .act(org_id.parse::<i64>().unwrap(), PanelV2Action::Create(req))
-                                    .await
-                                {
-                                    Ok(v) => {
-                                        ctrl.add_selected_panel(v);
-                                        panel_resource.restart();
-                                        popup_service.close();
-                                    }
-                                    Err(_) => {}
-                                };
-                            }
-                        }
-                    },
-                    oncancel: move |_e: MouseEvent| {
-                        popup_service.close();
-                    },
-                }
-            })
-            .with_id("create_panel")
-            .with_title(translates.create_new_panel);
-    }
+    //     popup_service
+    //         .open(rsx! {
+    //             CreatePanelModal {
+    //                 lang: self.lang,
+    //                 onsave: {
+    //                     let client = client.clone();
+    //                     let org_id = org_id.clone();
+    //                     move |req: PanelV2CreateRequest| {
+    //                         let client = client.clone();
+    //                         let org_id = org_id.clone();
+    //                         async move {
+    //                             match client
+    //                                 .act(org_id.parse::<i64>().unwrap(), PanelV2Action::Create(req))
+    //                                 .await
+    //                             {
+    //                                 Ok(v) => {
+    //                                     ctrl.add_selected_panel(v);
+    //                                     panel_resource.restart();
+    //                                     popup_service.close();
+    //                                 }
+    //                                 Err(_) => {}
+    //                             };
+    //                         }
+    //                     }
+    //                 },
+    //                 oncancel: move |_e: MouseEvent| {
+    //                     popup_service.close();
+    //                 },
+    //             }
+    //         })
+    //         .with_id("create_panel")
+    //         .with_title(translates.create_new_panel);
+    // }
 
-    pub fn add_selected_panel(&mut self, panel: PanelV2) {
-        let mut panels = (self.selected_panels)();
-        panels.push(panel.clone());
-        self.selected_panels.set(panels);
+    // pub fn add_selected_panel(&mut self, panel: PanelV2) {
+    //     let mut panels = (self.selected_panels)();
+    //     panels.push(panel.clone());
+    //     self.selected_panels.set(panels);
 
-        let mut maximum_count = (self.maximum_panel_count)();
-        maximum_count.push(panel.user_count);
-        self.maximum_panel_count.set(maximum_count);
+    //     let mut maximum_count = (self.maximum_panel_count)();
+    //     maximum_count.push(panel.user_count);
+    //     self.maximum_panel_count.set(maximum_count);
 
-        let mut members = (self.total_panel_members)();
-        members += panel.user_count;
-        self.total_panel_members.set(members);
-    }
+    //     let mut members = (self.total_panel_members)();
+    //     members += panel.user_count;
+    //     self.total_panel_members.set(members);
+    // }
 
     pub fn remove_selected_panel(&mut self, index: usize) {
         let mut panels = (self.selected_panels)();
@@ -304,5 +246,109 @@ impl Controller {
 
     pub fn back(&self) {
         self.nav.go_back();
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct PanelController {
+    lang: Language,
+    pub panels: Resource<QueryResponse<PanelV2Summary>>,
+    pub selected_panels: Signal<Vec<(PanelV2Summary, i64)>>,
+    popup_service: PopupService,
+    org_id: Memo<i64>,
+    pub total_panels: Memo<i64>,
+}
+
+impl PanelController {
+    pub fn new(lang: Language) -> std::result::Result<Self, RenderError> {
+        let login_service: LoginService = use_context();
+        let org_id = use_memo(move || login_service.get_selected_org().unwrap_or_default().id);
+
+        let panels = use_resource(move || {
+            let org_id = org_id();
+            let size = 100;
+
+            let client = PanelV2::get_client(&crate::config::get().api_url);
+
+            async move {
+                // FIMXE: fix to get total data
+                let query = PanelV2Query::new(size);
+                match client.query(org_id, query).await {
+                    Ok(d) => d,
+                    Err(e) => {
+                        tracing::error!("list panels failed: {e}");
+                        QueryResponse::default()
+                    }
+                }
+            }
+        });
+        let selected_panels = use_signal(|| vec![]);
+        let total_panels = use_memo(move || {
+            let mut total = 0;
+            for (_, num) in selected_panels().iter() {
+                total += num;
+            }
+
+            total
+        });
+
+        let ctrl = Self {
+            lang,
+            panels,
+            org_id,
+            selected_panels,
+            total_panels,
+            popup_service: use_context(),
+        };
+
+        Ok(ctrl)
+    }
+
+    pub fn refresh(&mut self) {
+        self.panels.restart();
+    }
+
+    pub fn open_create_panel_modal(&self) {
+        let mut popup_service = self.popup_service;
+        let mut panel_resource = self.panels;
+        let org_id = (self.org_id)();
+
+        let mut ctrl = self.clone();
+
+        popup_service
+            .open(rsx! {
+                CreatePanelModal {
+                    lang: self.lang,
+                    onsave: move |req: PanelV2CreateRequest| async move {
+                        let client = PanelV2::get_client(&crate::config::get().api_url);
+                        match client.act(org_id, PanelV2Action::Create(req)).await {
+                            Ok(v) => {
+                                ctrl.add_selected_panel(v.into());
+                                panel_resource.restart();
+                                popup_service.close();
+                            }
+                            Err(_) => {}
+                        };
+                    },
+                    oncancel: move |_e: MouseEvent| {
+                        popup_service.close();
+                    },
+                }
+            })
+            .with_id("create_panel");
+        // .with_title(translates.create_new_panel);
+    }
+
+    pub fn add_selected_panel(&mut self, panel: PanelV2Summary) {
+        self.selected_panels
+            .push((panel.clone(), panel.user_count as i64));
+    }
+
+    pub fn change_number_by_index(&mut self, index: usize, number: i64) {
+        let mut selected_panels = (self.selected_panels)();
+        if index < selected_panels.len() {
+            selected_panels[index].1 = number;
+            self.selected_panels.set(selected_panels);
+        }
     }
 }
--- packages/main-ui/src/pages/surveys/new/setting_panel.rs
+++ packages/main-ui/src/pages/surveys/new/setting_panel.rs
@@ -1,60 +1,224 @@
+use super::i18n::*;
 use dioxus::prelude::*;
-use dioxus_translate::Language;
-use models::PanelV2;
+use dioxus_translate::*;
+use num_format::{Locale, ToFormattedString};
 
-use crate::pages::surveys::{
-    components::setting::Setting, models::current_step::CurrentStep, new::controller::Controller,
+use crate::{
+    components::icons::{Clear, Remove},
+    pages::surveys::new::controller::*,
 };
 
-#[derive(Props, Clone, PartialEq)]
-pub struct SettingPanelProps {
+#[derive(Debug, Clone, PartialEq, Default)]
+pub struct PanelResponse {}
+
+#[component]
+pub fn SettingPanel(
     lang: Language,
     visibility: bool,
+
+    #[props(extends = GlobalAttributes)] attributes: Vec<Attribute>,
+    onback: EventHandler<()>,
+    onnext: EventHandler<PanelResponse>,
+) -> Element {
+    let mut is_open = use_signal(|| false);
+    let mut ctrl = PanelController::new(lang)?;
+
+    let mut selected_panels = ctrl.selected_panels;
+    let total_panels = ctrl.total_panels;
+    let translate: SettingPanelTranslate = translate(&lang);
+
+    rsx! {
+        div {
+            class: "flex flex-col w-full justify-start items-start",
+            ..attributes,
+            div { class: "flex flex-row w-full justify-between items-center mb-[10px]",
+                div { class: "font-medium text-black text-[16px] leading-[22.5px]",
+                    "{translate.composition_panel}"
+                }
+                button {
+                    class: "bg-[#2a60d3] rounded-4px px-[14px] py-[8px] font-semibold text-white text-[16px] rounded-[4px]",
+                    onclick: move |_| {
+                        ctrl.open_create_panel_modal();
+                    },
+                    "{translate.create_panel}"
+                }
+            }
+
+            div {
+                class: "flex flex-col w-full justify-start items-start px-[40px] py-[24px] bg-white rounded-[8px]",
+                style: "box-shadow: 0 4px 6px rgba(53, 70, 177, 0.05);",
+                div { class: "flex flex-col w-full justify-start items-start mb-[20px]",
+                    div { class: "font-bold text-[#222222] text-lg mb-[5px]",
+                        "{translate.total_panel_setting}"
+                    }
+                    div { class: "font-normal text-[#6d6d6d] text-[14px]",
+                        "{translate.total_panel_setting_description}"
+                    }
+                }
+
+                div { class: "flex flex-col w-full justify-start items-start",
+
+                    PanelSettingInput {
+                        label: "{translate.total_panel}",
+                        unit: "{translate.person}",
+                        value: total_panels(),
+                        oninput: move |_value: i64| {},
+                    }
+
+                    div { class: "flex flex-row w-full justify-between items-center",
+                        div { class: "flex flex-row w-[200px] font-medium text-[15px] text-black",
+                            "{translate.select_panel}"
+                        }
+                        div { class: "relative w-full",
+                            button {
+                                class: "flex flex-row w-full min-h-[55px] justify-start items-center bg-[#f7f7f7] rounded-[4px] p-[15px]",
+                                onclick: move |_| {
+                                    is_open.set(true);
+                                },
+                                if selected_panels.len() != 0 {
+                                    div { class: "flex flex-wrap flex-1 justify-start items-center gap-[5px]",
+                                        for (i , panel) in selected_panels.iter().enumerate() {
+                                            PanelLabel {
+                                                label: panel.0.name.clone(),
+                                                onclose: move |_| {
+                                                    selected_panels.remove(i);
+                                                },
+                                            }
+                                        }
+                                    }
+                                    button {
+                                        onclick: move |_| {
+                                            selected_panels.clear();
+                                        },
+                                        Remove {
+                                            width: "15",
+                                            height: "15",
+                                            fill: "#555462",
+                                        }
+                                    }
+                                }
+                            }
+                            if is_open() {
+                                div { class: "absolute flex flex-col w-full justify-start items-center shadow-[0px_8px_20px_rgba(20,26,62,0.25)] bg-white py-4 rounded-md",
+                                    div { class: "flex flex-row w-full justify-end px-[10px]",
+                                        button {
+                                            onclick: move |_| {
+                                                is_open.set(false);
+                                            },
+                                            Remove {
+                                                width: "15",
+                                                height: "15",
+                                                fill: "#555462",
+                                            }
+                                        }
+                                    }
+                                    if let Some(panels) = ctrl.panels.value()() {
+                                        for panel in panels.items {
+                                            if !selected_panels.iter().any(|selected| selected.0.name == panel.name) {
+                                                div {
+                                                    class: "flex flex-col w-full h-[60px] justify-start items-start py-[9px] bg-white hover:bg-[#f7f7f7] hover:border-l hover:border-l-[#2a60d3] cursor-pointer",
+                                                    onclick: move |_| {
+                                                        ctrl.add_selected_panel(panel.clone());
+                                                        is_open.set(false);
+                                                    },
+                                                    div { class: "flex flex-col w-full px-4",
+                                                        div { class: "font-bold text-[15px] text-[#222222] mb-[5px]",
+                                                            "{panel.name}"
+                                                        }
+                                                        div { class: "font-medium text-[10px] text-[#222222]",
+                                                            "{translate.total_people}: {panel.user_count}"
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+                div { class: "flex flex-col w-full justify-start items-start",
+                    div { class: "flex flex-row w-full h-[1px] bg-[#ebeff5] my-[20px]" }
+                    div { class: "flex flex-col w-full justify-start items-start gap-[10px]",
+                        for (i , sp) in selected_panels().iter().enumerate() {
+                            PanelSettingInput {
+                                label: "{sp.0.name}",
+                                unit: "{translate.person}",
+                                value: sp.1,
+                                oninput: move |value: i64| {
+                                    ctrl.change_number_by_index(i, value);
+                                },
+                            }
+                        }
+                    }
+                }
+            }
+
+            div { class: "flex flex-row w-full justify-end items-center gap-[20px] text-white mt-[40px]",
+                button {
+                    class: "px-[20px] py-[10px] border-[#BFC8D9] bg-white border-[1px] text-[#555462] font-semibold text-[14px] rounded-[4px]",
+                    onclick: move |_| {
+                        onback(());
+                    },
+                    "{translate.btn_cancel}"
+                }
+
+                button {
+                    class: "px-[20px] py-[10px] bg-[#2A60D3] font-semibold text-[14px] rounded-[4px]",
+                    onclick: move |_| async move {
+                        onnext(PanelResponse {});
+                    },
+                    "{translate.btn_complete}"
+                }
+            }
+        }
+    }
 }
 
 #[component]
-pub fn SettingPanel(props: SettingPanelProps) -> Element {
-    let mut ctrl: Controller = use_context();
-    let selected_panels = ctrl.selected_panels();
-    let panels = ctrl.total_panels();
-    let total_members = ctrl.get_total_panel_members();
+pub fn PanelSettingInput(
+    label: String,
+    unit: String,
+    value: i64,
+    oninput: EventHandler<i64>,
+) -> Element {
+    let mut value = use_signal(|| value);
 
     rsx! {
-        Setting {
-            visibility: if !props.visibility { "hidden" },
-            width: if !props.visibility { "0px" },
-            height: if !props.visibility { "0px" },
+        div { class: "flex flex-row w-full justify-between items-center",
+            div { class: "font-medium text-[#222222] text-[15px]", "{label}" }
+            div { class: "flex flex-row h-[55px] items-center gap-[10px]",
+                input {
+                    class: "flex flex-row w-[215px] h-[55px] justify-end items-center rounded-[4px] px-[15px] py-[10px] bg-[#f7f7f7] font-medium text-[#222222] text-[15px] text-right",
+                    r#type: "text",
+                    placeholder: "0",
+                    value: value().to_formatted_string(&Locale::en),
+                    oninput: move |e| {
+                        let v = e.value().parse::<i64>().unwrap_or(value());
+                        value.set(v);
+                        oninput.call(v);
+                    },
+                }
 
-            lang: props.lang,
-            total_members,
-            selected_panels,
-            panels,
-            maximum_counts: ctrl.maximum_counts(),
+                div { class: "font-normal text-black text-[15px]", "{unit}" }
+            }
+        }
+    }
+}
 
-            open_create_panel_modal: move |_| async move {
-                ctrl.open_create_panel_modal().await;
-            },
-            remove_selected_panel: move |index: usize| {
-                ctrl.remove_selected_panel(index);
-            },
-            remove_all_selected_panel: move |_| {
-                ctrl.remove_all_selected_panel();
-            },
-            add_selected_panel: move |panel: PanelV2| {
-                ctrl.add_selected_panel(panel);
-            },
-            change_selected_panel_count: move |(index, count): (usize, u64)| {
-                ctrl.change_selected_panel_count(index, count);
-            },
-            change_total_panel_members: move |members: u64| {
-                ctrl.change_total_panel_members(members);
-            },
-            change_step: move |step: CurrentStep| {
-                ctrl.change_step(step);
-            },
-            save_survey: move |_| async move {
-                ctrl.save_survey().await;
-            },
+#[component]
+pub fn PanelLabel(label: String, onclose: EventHandler<MouseEvent>) -> Element {
+    rsx! {
+        div { class: "flex flex-row h-[25px] justify-center items-center px-[8px] py-[3px] bg-[#35343f] rounded-[5px] gap-[10px]",
+            div { class: "font-semibold text-[14px] text-white", {label} }
+            button {
+                onclick: move |e: Event<MouseData>| {
+                    onclose.call(e);
+                },
+                Clear { width: "20", height: "20" }
+            }
         }
     }
 }
