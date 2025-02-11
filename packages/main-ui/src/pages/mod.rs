4e1a3c858dcf2ce3e48513434f04b5986c4cdf0c
hackartists <hackartists@gmail.com>
2025-02-11 11:24:18 +0900

    refactor: remove unused response report components and routes

--- packages/main-ui/src/pages/mod.rs
+++ packages/main-ui/src/pages/mod.rs
@@ -5,13 +5,6 @@ mod page;
 pub mod attributes;
 pub mod create;
 pub mod find_email;
-pub mod id {
-    pub mod response_report;
-    pub mod select_response;
-    pub mod survey_summary;
-    pub mod write_question;
-    pub mod write_title;
-}
 pub mod groups;
 pub mod members;
 pub mod not_found;
