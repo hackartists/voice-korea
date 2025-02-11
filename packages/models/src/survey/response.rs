95883e0d06fa501404c7f6f8e36786d316a2bdaf
hackartists <hackartists@gmail.com>
2025-02-11 14:53:07 +0900

    feat(survey): add SurveyResponse model with attributes and answers enums

---
+++ packages/models/src/survey/response.rs
@@ -0,0 +1,63 @@
+#![allow(unused)]
+use crate::{GenderV2, RegionV2, Result, SalaryV2};
+#[cfg(feature = "server")]
+use by_axum::aide;
+use by_macros::api_model;
+use by_types::QueryResponse;
+
+#[api_model(base = "/v2/surveys/:survey-id/responses", table = survey_response)]
+pub struct SurveyResponse {
+    #[api_model(summary, primary_key)]
+    pub id: i64,
+    #[api_model(summary, auto = [insert])]
+    pub created_at: i64,
+    #[api_model(summary, auto = [insert, update])]
+    pub updated_at: i64,
+
+    pub proof_id: String,
+
+    #[api_model(summary, type = JSONB)]
+    pub attributes: Vec<Attribute>,
+
+    #[api_model(summary, type = JSONB)]
+    pub answers: Vec<Answer>,
+
+    #[api_model(many_to_one = surveys)]
+    pub survey_id: i64,
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, serde::Serialize, serde::Deserialize)]
+#[cfg_attr(feature = "server", derive(schemars::JsonSchema, aide::OperationIo))]
+#[serde(rename_all = "snake_case", tag = "answer_type")]
+pub enum Answer {
+    SingleChoice { answer: i32 },
+    MultipleChoice { answer: Vec<i32> },
+    ShortAnswer { answer: String },
+    Subjective { answer: String },
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, serde::Serialize, serde::Deserialize, Default)]
+#[cfg_attr(feature = "server", derive(schemars::JsonSchema, aide::OperationIo))]
+#[serde(rename = "snake_case", tag = "type")]
+pub enum Attribute {
+    Age(AgeV3),
+    Gender(GenderV2),
+    Region(RegionV2),
+    Salary(SalaryV2),
+
+    #[default]
+    None,
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, serde::Serialize, serde::Deserialize, Default)]
+#[cfg_attr(feature = "server", derive(schemars::JsonSchema, aide::OperationIo))]
+#[serde(rename = "snake_case")]
+pub enum AgeV3 {
+    Specific(u8),
+    Range {
+        inclusive_min: u8,
+        inclusive_max: u8,
+    },
+    #[default]
+    None,
+}
